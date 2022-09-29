ddbc2224650a1cd0bb2fbe4818e5a8d8627e9c49
Nwsheats <nwsheats@gmail.com>
2022-09-29 18:33:14 -0500

    added HTML and javascript variables + a function

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ console.log()
 
 let dateToday = moment();
 $("#currentDay").text(dateToday.format("MMM Do, YYYY"));
+
+let scheduleBox = $('#schedule');
+let content = $('#content');
+let nineAM = $('#9');
+let tenAM = $('#10');
+let elevenAM = $('#11');
+let noon = $('#12');
+let onePM = $('#13');
+let twoPM = $('#14');
+let threePM = $('#15');
+let fourPM = $('#16');
+let fivePM = $('#17');
+
+
+function contentSubmit(event) {
+  event.preventDefault();
+  let dailyTask = $('input[name="content"]').val();
+  content.append('<li>' + dailyTask + '</li>');
+  $('input[name="content"]').val('');
+}
+
+scheduleBox.on('submit', contentSubmit);
